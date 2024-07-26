fad5b9c3ab3fba6ef4e43d21029202eed706a88a
Abhishek Verma <Abhishek.verma1@businessnext.com>
2024-07-26 11:58:45 +0000

    added support for multi port

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.BUILD_ENV
+    basePath: process.env.BUILD_ENV_DEV || process.env.BUILD_ENV_QA || process.env.BUILD_ENV_SA
 };
 
 export default nextConfig;
